ff428398a3dbfc33b5b9be059a25ff845daf8a14
Clarence Rouvel <clarence.rouvel@gmail.com>
2021-08-08 21:26:27 +0200

    first chart displayed ! :)

--- src/components/TypeChart.js
+++ src/components/TypeChart.js
@@ -1,22 +1,55 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Doughnut } from 'react-chartjs-2';
+
 
 const TypeChart = () => {
     const [chartData, setChartData] = useState({});
 
     const chart = () => {
-        axios
+        var typeNames = [];
+        var typeAmount = [];
+         return axios
         .get('http://localhost:5000/api/records/typescount')
-        .then(res => {console.log(res)})
+        .then(res => {
+            console.log(res.data);
+            for (const dataObj of res.data) {
+              typeNames.push(dataObj.gc_obo_type_c);
+              typeAmount.push(parseInt(dataObj.total));
+            }
+
+            console.log(typeNames);
+            console.log(typeAmount);
+            setChartData({
+                maintainAspectRatio: false,
+                responsive: false,
+                  datasets: [{
+                    data: typeAmount,
+                    backgroundColor: ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B','#FB3640', '#EFCA08', '#43AA8B', '#253D5B','#FB3640', '#EFCA08', '#43AA8B', '#253D5B','#FB3640', '#EFCA08', '#43AA8B', '#253D5B' ]
+                  }],
+                  labels: typeNames
+                
+            });
+           
+          })
         .catch(err => {console.log(err)})
     };
+    console.log(chartData);
 
     useEffect(() => {
         chart();
       }, []);
 
       return(
-          <div></div>
+          <Doughnut data={chartData}
+          options={{
+            responsive: true,
+            legend: {
+              position: 'bottom'
+            }
+          }}
+          />
+
       );
 };
 
